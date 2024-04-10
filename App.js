32ea70c353a3e7f714813dd5458ca8ed9c196ddb
David Minott <67799380+sepasfr@users.noreply.github.com>
2024-04-09 23:36:22 -0400

    Update App.js
    footer added

--- App.js
+++ App.js
@@ -17,20 +17,32 @@ return (
   <div className="image-container">
     <img src="/Man2.jpg" alt="Man working on Car" />
     <div className="text-over-image">Welcome to ShopWizard!</div>
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+  <button onClick={() => console.log('Get Started clicked')} style={{position: 'absolute', top: '20%', left: '44%', transform: 'translate(0%, 0%)', padding: '10px 20px', fontSize: '32px' }}> Get Started </button>
   </div>
+</div>
 );
-
 }
+
+
 const Mechanics = () => <div><h2>Mechanics</h2></div>;
 const ServiceTracking = () => <div><h2>Service Tracking</h2></div>;
 const Appointments = () => <div><h2>Appointments</h2></div>;
 const CustomerList = () => <div><h2>Customer List</h2></div>;
 const ServicePricing = () => <div><h2>Service Pricing</h2></div>;
+const Footer = () => (
+  <footer>
+    <p> Contact: </p>
+    <p>dm26690@georgiasouthern.edu</p>
+    <p>vl02201@georgiasouthern.edu</p>
+  </footer>
+);
+
 
 // main app component
 const App = () => {
   return (
-    <Router>
+    <><Router>
       <div>
         <nav>
           <ul>
@@ -54,7 +66,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
-    </Router>
+    </Router><Footer> </Footer></>
   );
 
 

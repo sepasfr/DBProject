1114fdeb115e7b7e4273836fa5307305d1d9a10e
sepasfr <david.minott04@gmail.com>
2024-04-11 20:28:19 -0400

    Update CustomerList.js

--- frontend/my-react-app/src/CustomerList.js
+++ frontend/my-react-app/src/CustomerList.js
@@ -11,7 +11,6 @@ const CustomerList = () => {
                 setCustomers(response.data);
             } catch (error) {
                 console.error('Error fetching customers:', error);
-                // Optionally handle error state
             }
         };
 
